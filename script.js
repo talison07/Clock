7d451d96e773c2a2f7203424d045f5d64378f273
Talison Antunes <talisonantunes.dev@gmail.com>
2022-08-23 23:26:11 -0300

    Added days of week

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let day = document.getElementById("day");
 let hours= document.getElementById("hours");
 let minutes= document.getElementById("minutes");
@@ -14,4 +15,34 @@ const relogio = setInterval(function time (){
     hours.textContent= hr;
     minutes.textContent = min;
     seconds.textContent= s;
+
+    if(d=1){
+        day.textContent ="Seg"
+    }
+
+    else if(d=2){
+        day.textContent ="Ter"
+    }
+
+    else if(d=3){
+        day.textContent ="Qua"
+    }
+
+    else if(d=4){
+        day.textContent ="Qui"
+    }
+
+    else if(d=5){
+        day.textContent ="Sex"
+    }
+
+    else if(d=6){
+        day.textContent ="Sab"
+    }
+
+    else if(d=7){
+        day.textContent ="Dom"
+    }
+
+
 })
